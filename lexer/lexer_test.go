3623a12fd3d533a04962dbec5fc0542719aaf760
nixiesquid <audu817@gmail.com>
2021-08-28 16:49:10 +0900

    implemented some keyword syntax interpretations in Lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -19,7 +19,11 @@ func TestNextToken(test *testing.T) {
 						!-/*5;
 						5 < 10 > 5;
 
-						return result;
+						if (5 < 10) {
+							return true;
+						} else {
+							return false;
+						}
 	`
 
 	tests := []struct {
@@ -90,10 +94,30 @@ func TestNextToken(test *testing.T) {
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 
-		// return result;
+		/*
+			if (5 < 10) {
+				return true;
+			} else {
+				return false;
+			}
+		*/
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
 		{token.RETURN, "return"},
-		{token.IDENT, "result"},
+		{token.TRUE, "true"},
 		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 
 		{token.EOF, ""},
 	}
--- token/token.go
+++ token/token.go
@@ -36,12 +36,20 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "true"
+	FALSE    = "false"
+	IF       = "if"
+	ELSE     = "else"
 	RETURN   = "return"
 )
 
 var keywords = map[string]TokenType{
 	"fn":     FUNCTION,
 	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
 	"return": RETURN,
 }
 
