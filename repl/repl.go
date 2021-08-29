cda4137ebe6e97a6f27bce51134e4635074ccf21
nixiesquid <audu817@gmail.com>
2021-08-29 18:34:10 +0900

    implement repl exit

--- repl/repl.go
+++ repl/repl.go
@@ -14,6 +14,7 @@ const PROMPT = ">> "
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
+outer:
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -25,6 +26,9 @@ func Start(in io.Reader, out io.Writer) {
 		lex := lexer.New(line)
 
 		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			if tok.Literal == "exit" {
+				break outer
+			}
 			fmt.Fprintf(out, "%+v\n", tok)
 		}
 	}
