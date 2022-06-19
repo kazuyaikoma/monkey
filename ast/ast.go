b273e9ab73e4912cd09902b43591827ad1980438
nixii <nixii0986@gmail.com>
2022-06-20 08:06:05 +0900

    :+1: add ast

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "github.com/nixii/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) == 0 {
+		return ""
+	} else {
+		return p.Statements[0].TokenLiteral()
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
